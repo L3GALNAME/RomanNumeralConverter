09518cdcf3e6085513826a1c62606db84ded93aa
L3GALNAME <supak00pa06@gmail.com>
2025-02-24 21:11:19 -0500

    Added styling and visuals
    Added colors, background shapes, a container for the main content, and JavaScript code to make the text shake

--- script.js
+++ script.js
@@ -6,18 +6,28 @@ const output = document.getElementById("output");
 function get_input() {
   // This is a separate function so value_check() can use return statements
   output.innerHTML = value_check(numberInput.value);
+  if (output.style.color === "#f00") {
+    output.style.color = "#ff0";
+    output.style.paddingLeft = "100px"
+    shake_offset(output, 5);
+  }
 }
 
 function value_check(val) {
   let numberVal = parseInt(val);
+  output.style.color = "#f00"; 
   if (val.toString() === "" || val%1 > 0) {
     // Checks if value is empty or contains a decimal
+    shake_offset(output, 15);
     return `<p>Please enter a valid number</p>`;
   } else if (numberVal < 1) {
+    shake_offset(output, 12);
     return `<p>Please enter a number greater than or equal to 1</p>`;
   } else if (numberVal > 3999) {
+    shake_offset(output, 15);
     return `<p>Please enter a number less than or equal to 3999</p>`;
   } else {
+    output.style.color = "#000"; 
     const romanValues = {
       M: 1000,
       CM: 900,
@@ -45,6 +55,17 @@ function value_check(val) {
     }
     
     return roman;
+  }
+}
+
+function shake_offset(element, amt=10, timer=0) {
+  element.style.paddingLeft = `${Math.sin(timer*1)*amt}px`
+
+  if (amt > 0) {
+    amt -= 1;
+    setTimeout(() => { shake_offset(element, amt, timer+1) }, 15);
+  } else {
+    element.style.color = "#000";
   }
 }
 
